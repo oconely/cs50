f0b4c9dba79f0cd9e79ff8a8ce8c721307cc5a18
oconely <and.ribaltovsky@gmail.com>
2022-02-13 17:03:15 +0300

    implement graph ds

--- data-structures/graph.js
+++ data-structures/graph.js
@@ -5,6 +5,96 @@
 // undirected graph
 class Graph {
   constructor() {
-    this.adjacencyList = {};
+    this.adjacencyList = {}
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = []
+    }
+  }
+  // add relationships between verted - edges
+  // drawing those connections between two vertices
+  addEdge(v1, v2){
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+  depthFirstRecursive(start){
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    (function dfs(vertex){
+      if (!vertex) {
+        return null;
+      };
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach(neighbor => {
+        if(!visited[neighbor]) {
+          return dfs(neighbor);
+        }
+      })
+    })(start);
+
+    return result; 
+  }
+  depthFirstIterative(start){
+    const stack = [start];
+    const result = [];
+    const visited = {};
+
+    let currentVertex;
+    
+    visited[start] = true;
+    while(stack.length) {
+      currentVertex = stack.pop();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
+  breadthFirst(start){
+    const queue = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+    visited[start] = true;
+
+    while(queue.length){
+      currentVertex = queue.shift();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      })
+    }
+    return result;
   }
 }
+
+let g = new Graph();
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
+
+g.depthFirstIterative('A')
